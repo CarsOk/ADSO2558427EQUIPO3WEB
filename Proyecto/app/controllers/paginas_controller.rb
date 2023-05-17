135251ba5f6f3b3bed69b99b61e91f88e07c1fc9
Juan Vides <juanvides112011@gmail.com>
2023-05-17 15:04:29 -0500

    Paginas

---
+++ Proyecto/app/controllers/paginas_controller.rb
@@ -0,0 +1,11 @@
+class PaginasController < ApplicationController
+    def tienda
+        
+    end
+    def acerca
+        
+    end
+    def contacto
+        
+    end
+end
--- Proyecto/config/routes.rb
+++ Proyecto/config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'home#dashboard'
+  root 'paginas#tienda'
+  get '/paginas/tienda', to: 'paginas#tienda', as: 'tienda'
+  get '/paginas/acerca', to: 'paginas#acerca', as: 'acerca'
+  get '/paginas/contacto', to: 'paginas#contacto', as: 'contacto'
 end
