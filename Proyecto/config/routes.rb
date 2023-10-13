93988a544026e43233692296da29e77648069b4c
Joseargote18 <110869965+Jxze18@users.noreply.github.com>
2023-10-13 16:27:42 -0500

    creacion de inicio de secion
    creacion del login y implementacion de estilos

--- Proyecto/config/routes.rb
+++ Proyecto/config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
-  devise_for :users
-  root 'home#dashboard'
+  
+  devise_for :users, controllers: {
+    sessions: 'custom_sessions'
+  }
+  root 'paginas#tienda'
+  get '/tienda', to: 'paginas#tienda', as: 'tienda'
+  get '/acerca', to: 'paginas#acerca', as: 'acerca'
+  get '/contacto', to: 'paginas#contacto', as: 'contacto'
+  get '/about', to: 'paginas#about', as: 'about'
+
+
+
 end
