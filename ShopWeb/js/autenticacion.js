8d80155ae40f712eacccef2d00e9ef1e47de0550
Joseargote18 <110869965+Jxze18@users.noreply.github.com>
2023-11-10 16:08:10 -0500

    live

---
+++ ShopWeb/js/autenticacion.js
@@ -0,0 +1,94 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const registerForm = document.getElementById("registerForm");
+
+    registerForm.addEventListener("submit", function (event) {
+        event.preventDefault();
+
+        // Obtener los valores del formulario
+        const name = document.getElementById("name").value;
+        const email = document.getElementById("email").value;
+        const password = document.getElementById("password").value;
+
+        // Crear un objeto con los datos del usuario
+        const user = {
+            name: name,
+            email: email,
+            password: password,
+        };
+
+        // Realizar la solicitud Fetch para enviar los datos al servidor
+        fetch("http://127.0.0.1:4000/CrearUsuario", {
+            method: "POST",
+            mode: "cors",
+            credentials: "include",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(user),
+        })
+            .then((response) => response.json())
+            .then((data) => {
+                // Manejar la respuesta del servidor (puede ser un mensaje de éxito o error)
+                if (data.success) {
+                    // Registro exitoso, puedes redirigir al usuario o mostrar un mensaje de éxito
+                    alert("Registro exitoso");
+                } else {
+                    // Mostrar un mensaje de error
+                    alert("Error en el registro: " + data.message);
+                }
+            })
+            .catch((error) => {
+                // Manejar errores de la solicitud Fetch
+                console.error("Error en la solicitud Fetch: " + error);
+            });
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    const loginForm = document.getElementById("loginForm");  // Asegúrate de tener un formulario con el id "loginForm" en tu HTML.
+
+    registerForm.addEventListener("submit", function (event) {
+        event.preventDefault();
+        // ... (código para el registro, igual al que proporcionaste)
+
+    });
+
+    loginForm.addEventListener("submit", function (event) {
+        event.preventDefault();
+
+        // Obtener los valores del formulario de inicio de sesión
+        const username = document.getElementById("username").value;
+        const password = document.getElementById("password").value;
+
+        // Crear un objeto con los datos del usuario para iniciar sesión
+        const userLogin = {
+            correo: username,
+            password: password,
+        };
+
+        // Realizar la solicitud Fetch para enviar los datos al servidor para iniciar sesión
+        fetch("http://127.0.0.1:4000/IniciarSesion", {
+            method: "POST",
+            mode: "cors",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(userLogin),
+        })
+            .then((response) => response.json())
+            .then((data) => {
+                // Manejar la respuesta del servidor para iniciar sesión
+                if (data.success) {
+                    // Inicio de sesión exitoso, puedes redirigir al usuario o mostrar un mensaje de éxito
+                    alert("Inicio de sesión exitoso");
+                } else {
+                    // Mostrar un mensaje de error
+                    alert("Error en el inicio de sesión: " + data.message);
+                }
+            })
+            .catch((error) => {
+                // Manejar errores de la solicitud Fetch
+                console.error("Error en la solicitud Fetch: " + error);
+            });
+    });
+});
